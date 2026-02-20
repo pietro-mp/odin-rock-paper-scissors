0d760c220583a64f3f8972c1014fe2826b24c5c6
pietro-mp <148398502+pietro-mp@users.noreply.github.com>
2026-02-20 01:17:38 -0300

    add playRound and convertChoiceToInt

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // TODO: return "rock", "paper" or "scissors", randomly
     // i can use Math.random
@@ -13,4 +16,37 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     return prompt('choose between \'rock\', \'paper\' or \'scissors\'');
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = convertChoiceToInt(humanChoice.toLowerCase());
+    computerChoice = convertChoiceToInt(computerChoice);
+
+    // tie = 0;
+    // player wins = 1;
+    // player loses = 2
+    switch ((humanChoice - computerChoice + 3) % 3) {
+        case 0:
+            console.log("It's a tie!");
+            break;
+        case 1:
+            humanScore++;
+            console.log("You won!");
+            break;
+        case 2:
+            computerScore++;
+            console.log("You lost (the game)!");
+            break;
+    }
+}
+
+function convertChoiceToInt(choice) {
+    switch (choice) {
+        case 'rock':
+            return 0;
+        case 'paper':
+            return 1;
+        case 'scissors':
+            return 2;
+    }
 }
