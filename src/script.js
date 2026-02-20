ab58ef6ced4af3c1ee151964657709bfad9c769b
pietro-mp <148398502+pietro-mp@users.noreply.github.com>
2026-02-20 00:42:42 -0300

    add computer choice function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+function getComputerChoice() {
+    // TODO: return "rock", "paper" or "scissors", randomly
+    // i can use Math.random
+    switch (Math.floor(Math.random * 3)) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+console.log(getComputerChoice());
