206bada74ec59afa33bce7c950fc6de431e2543f
pietro-mp <148398502+pietro-mp@users.noreply.github.com>
2026-03-07 00:21:12 -0300

    final version of RPS game

--- src/script.js
+++ src/script.js
@@ -12,17 +12,53 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = convertChoiceToInt(humanChoice.toLowerCase());
-    computerChoice = convertChoiceToInt(computerChoice);
+    // os valores em int significam:
+    // pedra: 0
+    // papel: 1
+    // tesoura: 2
+    const humanChoiceInt = convertChoiceToInt(humanChoice.toLowerCase());
+    const computerChoiceInt = convertChoiceToInt(computerChoice);
 
-    // tie = 0;
-    // player wins = 1;
+    // o cálculo resulta em: 
+    // tie = 0
+    // player wins = 1
     // player loses = 2
-    switch ((humanChoice - computerChoice + 3) % 3) {
-        case 1: 
-            break;
-        case 2:
-            break;
+    if (!matchEnded) {
+        humanText.children[0].textContent = emojis[humanChoiceInt];
+        computerText.children[0].textContent = emojis[computerChoiceInt];
+        switch ((humanChoiceInt - computerChoiceInt + 3) % 3) {
+            case 0:
+                scoreText.children[0].textContent = 'It\'s a tie!';
+                scoreText.children[1].textContent = `${capitalize(humanChoice)} ties with ${capitalize(humanChoice)}.`
+                break;
+            case 1:
+                scoreText.children[0].textContent = 'You won!';
+                scoreText.children[1].textContent = `${capitalize(humanChoice)} beats ${computerChoice}.`;
+                humanScore.classList.add('highlight');
+                humanScore.textContent = +humanScore.textContent + 1;
+                setTimeout(() => {
+                    humanScore.classList.remove('highlight');
+                }, 300);
+                break;
+            case 2:
+                scoreText.children[0].textContent = 'You lost!';
+                scoreText.children[1].textContent = `${capitalize(computerChoice)} beats ${humanChoice}.`;
+                computerScore.textContent = +computerScore.textContent + 1;
+                computerScore.classList.add('highlight');
+                setTimeout(() => {
+                    computerScore.classList.remove('highlight');
+                }, 300);
+                break;
+        }
+    }
+
+    if (+humanScore.textContent >= 5) {
+        declareWinner('human');
+        return;
+    }
+    if (+computerScore.textContent >= 5) {
+        declareWinner('computer');
+        return;
     }
 }
 
@@ -36,3 +72,66 @@ function convertChoiceToInt(choice) {
             return 2;
     }
 }
+
+function declareWinner(winner) {
+    modal.style.display = 'block';
+    setTimeout(() => {
+        modalContent.classList.add('open');
+    }, 10);
+    if (winner === 'human') {
+        winnerText.textContent = 'You won!';
+    }
+
+    else if (winner === 'computer') {
+        winnerText.textContent = 'You lost...';
+    }
+    
+    matchEnded = true;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function resetStatus() {
+    humanScore.textContent = 0;
+    computerScore.textContent = 0;
+    humanText.children[0].textContent = '?';
+    computerText.children[0].textContent = '?';
+    scoreText.children[0].textContent = 'Choose below to start';
+    scoreText.children[1].textContent = 'The first to get 5 points win';
+    matchEnded = false;
+}
+
+const scoreText = document.querySelector(".scoreSection .text");
+
+const humanText = document.querySelector(".human");
+const computerText = document.querySelector(".computer")
+
+const humanScore = document.querySelector(".human .score > span")
+const computerScore = document.querySelector(".computer .score > span")
+
+const options = document.querySelector(".options");
+const emojis = ['👊', '🖐️', '✌️'];
+const moves = ['rock', 'paper', 'scissors'];
+
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const closeModal = document.querySelector("#again");
+const winnerText = document.querySelector(".modal-content > p");
+let matchEnded = false;
+
+document.addEventListener("click", function (event) {
+    const id = event.target.id;
+
+    if (moves.includes(id)) {
+        playRound(id, getComputerChoice());
+    }
+    else if (id === 'again' || event.target === modal) {
+        modalContent.classList.remove('open');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 200);
+        if (id === 'again') resetStatus();
+    }
+});
